b02083876e9ce768865dd0d113ccf0757111635b
janithnishel <dhjanithnishel@gmail.com>
2021-02-24 03:06:27 +0530

    first new class

---
+++ lab1/src/lab1/ex.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first project at SLIIT");
+
+	}
+
+}
